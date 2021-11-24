8594fdaf99358e29156fa9dba0913dce4c2e6a30
Tiendinh <tiendinh1352@gmail.com>
2021-11-24 23:53:19 +0700

    update frontend

---
+++ src/main/frontend/src/api/itemApi.js
@@ -0,0 +1,12 @@
+import axiosClient from "./axiosClient";
+
+
+const itemApi ={
+    getAll: ()=>{
+        const url='/user/items/get';
+        return axiosClient.get(url);
+},
+
+}
+
+export default itemApi;
--- src/main/java/com/Springboot/aha/Security/SecurityConfig.java
+++ src/main/java/com/Springboot/aha/Security/SecurityConfig.java
@@ -1,21 +1,16 @@
 package com.Springboot.aha.Security;
 
-import com.Springboot.aha.Entity.ERole;
 import com.Springboot.aha.Service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -27,33 +22,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthEntryPoint unauthorizedHandler;
 
-    @Bean
-    public AuthenticationFilter authenticationFilter() {
-        return new AuthenticationFilter();
-    }
+//    @Bean
+//    public AuthenticationFilter authenticationFilter() {
+//        return new AuthenticationFilter();
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+//    }
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                .sessionManagement().sessionCreationPolicy(STATELESS).and()
-                .authorizeRequests().antMatchers("/auth/**").permitAll().and()
-                .authorizeRequests().antMatchers("/admin/**").hasAuthority(String.valueOf(ERole.ROLE_ADMIN)).and()
-                .authorizeRequests().antMatchers("/user/**").hasAuthority(String.valueOf(ERole.ROLE_USER))
-        ;
-        http.authorizeRequests().anyRequest().authenticated();
-        http.addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.cors().and().csrf().disable().authorizeRequests().anyRequest().permitAll();
+//                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+//                .sessionManagement().sessionCreationPolicy(STATELESS).and()
+//                .authorizeRequests().antMatchers("/auth/**").permitAll().and()
+//                .authorizeRequests().antMatchers("/api/admin/**").hasAuthority(String.valueOf(ERole.ROLE_ADMIN)).and()
+//                .authorizeRequests().antMatchers("/api/user/**").hasAuthority(String.valueOf(ERole.ROLE_USER))
+//        ;
+//        http.authorizeRequests().anyRequest().authenticated();
+//        http.addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
 
