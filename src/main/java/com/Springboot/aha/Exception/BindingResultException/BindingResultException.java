651b4466ed013039724a13e77167da046f2c692b
Tiendinh <1801040189@s.hanu.edu.vn>
2024-01-28 19:09:34 +0700

    remove frontend folder

--- src/main/java/com/Springboot/aha/Exception/BindingResultException/BindingResultException.java
+++ src/main/java/com/Springboot/aha/Exception/BindingResultException/BindingResultException.java
@@ -6,6 +6,6 @@ import java.util.stream.Collectors;
 
 public class BindingResultException {
     public static String getErrorMessage(BindingResult bindingResult) {
-        return bindingResult.getAllErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.joining(" ,")).toString();
+        return bindingResult.getAllErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.joining(" ,"));
     }
 }
